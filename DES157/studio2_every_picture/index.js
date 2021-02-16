763d0d23e9f241df40e030ed7bff8cecfe6521b9
Cory Risch-Smith <coryrischsmith1201@gmail.com>
2021-02-16 01:45:21 -0800

    style changes

--- DES157/studio2_every_picture/index.js
+++ DES157/studio2_every_picture/index.js
@@ -1,9 +1,24 @@
 (function () {
-  'use strict';
+  ('use strict');
 
-  // document.addEventListener('mouseover', function(){
+  //Red Color Block
+  const colorBlock = document.querySelector('.cBlockOne');
 
-  // });
+  colorBlock.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.getElementById('colorBlockOne').className = 'cBlockOne animate';
+  });
+
+  //Orange Color Block
+
+  //Yellow Color Block
+
+  //Green Color Block
+
+  //Blue Color Block
+
+  //Brown Color Block
 
   console.log('test');
 })();
