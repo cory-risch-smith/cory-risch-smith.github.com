61d19adc71dd9c64e04f61311d0d1f4e4ea83125
Cory Risch-Smith <coryrischsmith1201@gmail.com>
2021-03-13 01:18:25 -0800

    updated styling

--- DES157/final/proto/index.js
+++ DES157/final/proto/index.js
@@ -14,6 +14,10 @@
   var finalBtn = document.getElementById('finalBtn');
   var namiOverlay = document.getElementById('namiOverlay');
 
+  for (i = 0; i < boxes.length; i++) {
+    boxes[i].classList.add('hide');
+  }
+
   //sets the initial div to small size in viewport
 
   // (initial (init) values go in css, changes go in the js)
@@ -28,62 +32,48 @@
     scrollPos = [window.scrollY];
     console.log(scrollPos);
 
-    // Handles 1st div
-    if (scrollPos >= 0 && scrollPos <= 6000) {
-
+    //  Handles first Voice
+    if (scrollPos >= 0 && scrollPos <= 3000) {
       var myScale = scrollPos / 1000;
       // console.log(myScale);
 
-      box0.style.transform = `matrix(${myScale},0,0,${myScale},${myScale},${myScale})`;
+      boxes[0].style.transform = `matrix(${myScale},0,0,${myScale},${myScale},${myScale})`;
 
-      box0.style.opacity = `${myScale}`;
-
-      box0.style.zIndex = '1';
-
-      v0.style.opacity = `${myScale * 0.5}`;
+      v0.style.opacity = `${myScale}`;
+      boxes[0].classList.add('show');
     }
 
-    // Handles 2nd div
+    // Handles 2nd Voice
 
     // *******************************************************************change to the text and take opacity off the box and put on the type
-    else if (scrollPos >= 6001 && scrollPos <= 12000) {
-      box0.style.visibility = 'hidden';
-      box2.style.visibility = 'visible';
+    else if (scrollPos >= 3000 && scrollPos <= 6000) {
+      // boxes[0].classList.add('hide');
+      boxes[1].classList.add('show');
 
-      myScale = scrollPos / 1000 - 6;
+      myScale = scrollPos / 1000 - 3;
 
       // console.log(myScale);
 
-      box2.style.transform = `matrix(${myScale},0,0,${myScale},${myScale},${myScale})`;
+      // As matrix comes in, fade opacity of previous text
+      v0.style.opacity = `${1 - myScale * 2.5}`;
 
-      box2.style.opacity = `${myScale}`;
+      boxes[1].style.transform = `matrix(${myScale},0,0,${myScale},${myScale},${myScale})`;
 
-      box2.style.zIndex = '1';
-
-      v0.style.visibility = 'hidden';
-      v1.style.visibility = 'visible';
       v1.style.opacity = `${myScale * 0.5}`;
     }
     // Handles 3rd div
-    else if (scrollPos >= 12001 && scrollPos <= 24000) {
-      box2.style.visibility = 'hidden';
-      box3.style.visibility = 'visible';
-
-      myScale = scrollPos / 1000 - 12;
+    else if (scrollPos >= 3001 && scrollPos <= 24000) {
+      boxes[1].classList.add('hide');
+      boxes[2].classList.add('show');
+      myScale = scrollPos / 1000 - 3;
 
       // console.log(myScale);
 
-      box3.style.transform = `matrix(${myScale / 10},0,0,${myScale / 10},${
+      boxes[2].style.transform = `matrix(${myScale / 10},0,0,${myScale / 10},${
         myScale / 10
       },${myScale / 10})`;
 
-      box3.style.opacity = `${myScale}`;
-
-      box3.style.zIndex = '1';
-
-      v1.style.visibility = 'hidden';
-      v2.style.visibility = 'visible';
-      box4.style.visibility = 'hidden';
+      boxes[2].style.opacity = `${myScale}`;
     }
 
     // Handles 4th div, Vertical Scroll
@@ -103,45 +93,45 @@
       // myScale = scrollPos / 1000 - 20;
       // console.log(myScale);
 
+      box3.classList.add('vertical');
+      box3.style.top = '24002px';
+
       box4.classList.add('vertical');
-      box4.style.top = '24002px';
+      box4.style.top = '26002px';
 
       box5.classList.add('vertical');
-      box5.style.top = '26002px';
+      box5.style.top = '28002px';
 
       box6.classList.add('vertical');
-      box6.style.top = '28002px';
+      box6.style.top = '30002px';
 
       box7.classList.add('vertical');
-      box7.style.top = '30002px';
+      box7.style.top = '32002px';
 
       box8.classList.add('vertical');
-      box8.style.top = '32002px';
+      box8.style.top = '34002px';
 
       box9.classList.add('vertical');
-      box9.style.top = '34002px';
+      box9.style.top = '36002px';
 
       box10.classList.add('vertical');
-      box10.style.top = '36002px';
+      box10.style.top = '38002px';
 
       box11.classList.add('vertical');
-      box11.style.top = '38002px';
+      box11.style.top = '40002px';
 
       box12.classList.add('vertical');
-      box12.style.top = '40002px';
-
-      box13.classList.add('vertical');
-      box13.style.top = '42002px';
-      box13.style.height = '768px';
+      box12.style.top = '42002px';
+      box12.style.height = '768px';
     }
   }
 
   finalBtn.addEventListener('click', function () {
-    box13.classList.add('fadeOut');
-    box13.style.visibility = 'hidden';
-    box14.style.visibility = 'visible';
-    box14.style.display = 'block';
-    box14.style.height = '100%';
+    box12.classList.add('fadeOut');
+    box12.style.visibility = 'hidden';
+    box13.style.visibility = 'visible';
+    box13.style.display = 'block';
+    box13.style.height = '100%';
     // **********FIX ME *****************
 
     // remove eventListener here
